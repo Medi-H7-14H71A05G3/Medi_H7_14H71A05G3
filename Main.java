23ad864e81821f3840866482d2043e49e7af3bfd
Medi-H7-14H71A05G3 <32010093+Medi-H7-14H71A05G3@users.noreply.github.com>
2017-09-16 15:35:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+                                        Student st1= new Student(01,"Swathi",new Date(1997,06,10),63.568);
+		Student st2 = new Student(02,"Swathi",new Date(1997,06,10),63.568);
+		Student st3 = new Student(03,"Swathi",new Date(1997,06,10),63.568);
+		Student st4 = new Student(04,"Swathi",new Date(1997,06,10),63.568);
+		
+
+	}
+
+}
